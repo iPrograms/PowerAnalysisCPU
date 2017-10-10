f1647097966f23e6f34494c0458bd3403b052793
= <=>
2017-10-10 10:51:28 -0700

    Basic Encryption done

--- rc4.py
+++ rc4.py
@@ -1,5 +1,7 @@
 
 import os
+import binascii
+import sys
 
 # Initialization vector [0,1,...256]
 s = []
@@ -7,19 +9,18 @@ s = []
 # Private key to initialy permute initialization vector S
 k = '12139757927403241289018348180751234973456123041823903030394848210389384746819101838437128191'
 
-
 t = []
 
+# Populate S with data 0,...256
 def initializeStateVector():
         for i in range(0,256):
                 s.append(i)
-                
                 # Expand key to the same lenth as S
                 t.append(int(k[i % len(k) ]))
         print('S: ',s)
 
+# Swap S data for permutaion
 def swap(i,j):
-
 	temp = s[i]
 	s[i] = s[j]
 	s[j] = temp
@@ -33,20 +34,40 @@ def initPermutationOfS():
             
         print('Permuted S: ',s)
 
-def encrypt(data,key):
+# Encrypt M byte of data with stream key
+def encrypt(data,key,filename):
 
-        print('** ', type(data))
-        print('-->',type(key))
+        data_to_int = int(data,16)
 
+        xor = data_to_int ^ key
 
-        # Chagne to int
-        #decode = data.decode('utf-8')
-        #decode = int(decode)
+        encrypted_hex = hex(xor)
 
-        #zor = bin( decode ^ key )
+        # Write hex value to file 
+        writeToFile(filename, encrypted_hex,'enc')
+
+# Write M byte of data to file
+def writeToFile(file,data,enc):
+
+        # Encrypt 
+        if enc == 'enc':
         
-        #print('data', decode, '^', key , '->', zor)
-            
+                # Change file extension
+                file_with_ext = file + '.encrypted'
+
+                #print(file_with_ext)
+                # Open if exists, otherwise create new
+                with open(file_with_ext, 'w+') as ef:
+                        ef.write(data)
+                ef.close()
+
+        # Decrypt
+        if enc == 'dec':
+                file_with_original_ext = file.replace('.encrypted','')
+                print(file_with_original_ext)
+
+
+# Open data stream from file, or any other type of data
 def getStreamData(streamData):
 
         try:
@@ -57,25 +78,34 @@ def getStreamData(streamData):
 
                         while True:
 
+                                # Extract a block of 8 bytes from streamData to encrpt
 
-                                # Extract a block of 1 byte from streamData to encrpt
-
-                                chunk = file.read(1)
+                                chunk = binascii.hexlify(file.read(8))
 
                                 if not chunk:
                                         break
-                                        
+
+                                # Keep generating key 
                                 a = ( a + 1 ) % 256
                                 b = ( b + s[b] )  % 256
                                 swap( s[ a ], s[ b ])
                                 te = ( s[ a ] + s[ b ] ) % 256
+
+                                # Encryption key
                                 round_key = s[te]
 
-                                print('Stream Key:  > ',bytes([ round_key ]) )
-
                                 # Encrypt chunk with stream key
+ 
+                                encrypt(chunk,round_key,file.name)
 
-                                print(encrypt(chunk,bytes([round_key])))
+                        # Double check encrypted file exists
+                        with open(file.name + '.encrypted','rb') as encrypted_f:
+                                if encrypted_f:
+                                        print('Done hacker!')
+                                        print('Encrypted content -> ', encrypted_f.name)
+                        encrypted_f.close()
+                        
+                        
         except IOError as e:
                 print('I/O Error!')
         except ValueError as va:
@@ -83,7 +113,8 @@ def getStreamData(streamData):
         except:
                 print('Unknown Error')
                 raise
-        
+
+                                                 
 initializeStateVector()
 print()
 print()
@@ -92,7 +123,9 @@ initPermutationOfS()
 print()
 print()
 
-getStreamData('/Users/user/Desktop/Screen Shot 2017-10-09 at 8.00.20 PM.png')
+# file to encrypt, needs abosolute path with extension
+
+getStreamData('/Users/user/Desktop/n.odt')
 
         
     
